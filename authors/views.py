094317cfe76c4813838126df317d28bb8060c0f5
enzoshimizu <eshimizu@gmail.com>
2024-02-19 22:18:21 -0300

    132. Enviando POST para outra URL e usando SESSION para salvar valores entre requests

--- authors/views.py
+++ authors/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 from .forms import RegisterForm
 
 
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+
+    form = RegisterForm(register_form_data)
 
     context = {
         'form': form,
     }
     return render(request, 'authors/pages/register_view.html', context)
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(request.POST)
+
+    return redirect('authors:register')
