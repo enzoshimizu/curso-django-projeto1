f4efd3e9302c1a7e6296768ea9ae772d07a9df4a
enzoshimizu <eshimizu@gmail.com>
2024-02-19 21:53:47 -0300

    131. Adicionando todos os campos manualmente para melhor controle do CSS

--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,11 @@ from .forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm()
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+
     context = {
         'form': form,
     }
