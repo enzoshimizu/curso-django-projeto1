a6f43ec6675a3e73d6161207f199b84d198251e0
enzoshimizu <eshimizu@gmail.com>
2024-02-21 19:20:18 -0300

    135. Sobrescrevendo campos do formulário completamente

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,39 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -27,15 +59,9 @@ class RegisterForm(forms.ModelForm):
                 'required': 'This field must not be empty',
                 'max_length': "This field must have less than 65 characters",
             },
-            'password': {
-                'required': 'This field must not be empty',
-            },
         }
         widgets = {
             'first_name': forms.TextInput(attrs={
                 'placeholder': 'Type your first name here.'
             }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Type your password here.'
-            })
         }
