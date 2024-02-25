9ca86d752bf4b4668605a667543def797a96b194
enzoshimizu <eshimizu@gmail.com>
2024-02-25 14:55:11 -0300

    167. Agrupando testes funcionais com pytest markers

---
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+import pytest
+
+from .base import RecipeBaseFunctionalTest
+
+
+@pytest.mark.functional_test
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    def test_recipe_home_page_without_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
+
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+
+        self.assertIn('No recipes found!', body.text)
