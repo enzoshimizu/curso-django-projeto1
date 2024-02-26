2da30b0d4991299c32adb2dc1d61b5444aa2135a
enzoshimizu <eshimizu@gmail.com>
2024-02-26 12:49:43 -0300

    180. Criando a página inicial da dashboard (admin do autor)

--- authors/views.py
+++ authors/views.py
@@ -59,8 +59,6 @@ def login_create(request):
 
     form = LoginForm(request.POST)
 
-    login_url = reverse('authors:login')
-
     if form.is_valid():
         authenticated_user = authenticate(
             username=form.cleaned_data.get('username', ''),
@@ -75,7 +73,7 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
@@ -92,3 +90,8 @@ def logout_view(request):
     logout(request)
 
     return redirect(reverse('authors:login'))
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
