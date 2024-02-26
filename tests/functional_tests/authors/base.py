f4f1ce28dc29d88210bd9a10d8fdafe5cd5b081b
enzoshimizu <eshimizu@gmail.com>
2024-02-25 21:24:12 -0300

    175. Testando o formulário de login

--- tests/functional_tests/authors/base.py
+++ tests/functional_tests/authors/base.py
@@ -1,7 +1,7 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
 import time
 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
 from utils.browser import make_chrome_browser
 
 
@@ -11,8 +11,13 @@ class AuthorsBaseTest(StaticLiveServerTestCase):
         return super().setUp()
 
     def tearDown(self) -> None:
-        self.browser.close()
+        self.browser.quit()
         return super().tearDown()
 
-    def sleep(self, seconds=5):
-        time.sleep(seconds)
+    def sleep(self, qtd=10):
+        time.sleep(qtd)
+
+    def get_by_placeholder(self, web_element, placeholder):
+        return web_element.find_element(
+            By.XPATH, f'//input[@placeholder="{placeholder}"]'
+        )
