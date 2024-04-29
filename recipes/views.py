cde109961443fb6a5ad104ff65eee131ae7bcf3d
enzoshimizu <eshimizu@gmail.com>
2024-04-28 21:09:04 -0300

    API

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
+from django.forms.models import model_to_dict
 from django.db.models import Q
 from django.http import Http404
+from django.http import JsonResponse
 from django.views.generic import ListView, DetailView
 
 from recipes.models import Recipe
@@ -47,6 +49,19 @@ class RecipeListViewBase(ListView):
 
 class RecipeListViewHome(RecipeListViewBase):
     template_name = 'recipes/pages/home.html'
+
+
+class RecipeListViewHomeApi(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.object_list.values()
+        recipe_dict = list(recipe)
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False
+        )
 
 
 class RecipeListViewCategory(RecipeListViewBase):
@@ -134,3 +149,26 @@ class RecipeDetail(DetailView):
         ctx.update({'is_detail_page': True})
 
         return ctx
+
+
+class RecipeDetailAPI(RecipeDetail):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+        del recipe_dict['preparation_step_is_html']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
