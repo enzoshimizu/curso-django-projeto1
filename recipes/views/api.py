a9bfc6e74a5c32cfd8fbccf8399506d863d49c3f
enzoshimizu <eshimizu@gmail.com>
2024-06-22 18:17:58 -0300

    Permissions

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,11 +1,12 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from recipes.models import Recipe
+from recipes.permissions import IsOwner
 from recipes.serializers import RecipeSerializer, TagSerializer
 from tag.models import Tag
 
@@ -18,9 +19,7 @@ class RecipeAPIV2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIV2Pagination
-    permission_classes = [
-        IsAuthenticated,
-    ]
+    permission_classes = [IsAuthenticatedOrReadOnly,]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -31,6 +30,23 @@ class RecipeAPIV2ViewSet(ModelViewSet):
             qs = qs.filter(category_id=category_id)
 
         return qs
+
+    def get_object(self):
+        pk = self.kwargs.get('pk', '')
+
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+        return super().get_permissions()
 
 
 @api_view()
